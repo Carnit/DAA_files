f848efecd3e8924cf290aa0893ac9a8bdcfdf75f
Carnit <nlesh.barat10.8@gmail.com>
2024-08-13 14:29:38 +0530

    changes made to LAB_1/LAB_1.py: gave diffrent test cases for both sorting strategies

--- LAB_1/LAB_1.py
+++ LAB_1/LAB_1.py
@@ -24,31 +24,41 @@ def bubble_sort(arr: list[int]) -> list[int]:
     return arr
 
 def main()-> None:
-    test_cases:dict = {}
+    test_cases1:dict = {}
+    test_cases2:dict = {}
 
-    test_cases['10'] = [r.randint(0, 100000) for i in range(10)]  
-    test_cases['20'] = [r.randint(0, 100000) for i in range(20)] 
-    test_cases['30'] = [r.randint(0, 100000) for i in range(30)] 
-    test_cases['40'] = [r.randint(0, 100000) for i in range(40)] 
-    test_cases['50'] = [r.randint(0, 100000) for i in range(50)] 
-    test_cases['100'] = [r.randint(0, 100000) for i in range(100)] 
-    test_cases['1000'] = [r.randint(0, 100000) for i in range(1000)]
-    test_cases['10000'] = [r.randint(0, 100000) for i in range(10000)]
+    test_cases1['10'] = [r.randint(0, 100000) for i in range(10)]  
+    test_cases1['20'] = [r.randint(0, 100000) for i in range(20)] 
+    test_cases1['30'] = [r.randint(0, 100000) for i in range(30)] 
+    test_cases1['40'] = [r.randint(0, 100000) for i in range(40)] 
+    test_cases1['50'] = [r.randint(0, 100000) for i in range(50)] 
+    test_cases1['100'] = [r.randint(0, 100000) for i in range(100)] 
+    test_cases1['1000'] = [r.randint(0, 100000) for i in range(1000)]
+    test_cases1['10000'] = [r.randint(0, 100000) for i in range(10000)]
 
-    x_values = list(test_cases.keys())
+    test_cases2['10'] = [r.randint(0, 100000) for i in range(10)]  
+    test_cases2['20'] = [r.randint(0, 100000) for i in range(20)] 
+    test_cases2['30'] = [r.randint(0, 100000) for i in range(30)] 
+    test_cases2['40'] = [r.randint(0, 100000) for i in range(40)] 
+    test_cases2['50'] = [r.randint(0, 100000) for i in range(50)] 
+    test_cases2['100'] = [r.randint(0, 100000) for i in range(100)] 
+    test_cases2['1000'] = [r.randint(0, 100000) for i in range(1000)]
+    test_cases2['10000'] = [r.randint(0, 100000) for i in range(10000)]
+
+    x_values = list(test_cases1.keys())
     y_values_bubble = []
     y_values_selection = []
 
     print('bubble sort')
     
-    for key, value in test_cases.items():
+    for key1, value1 in test_cases1.items():
 
         t1 = Timer()
         t1.start()
-        bubble_sort(value)
+        bubble_sort(value1)
         t1.stop()
 
-        print(f'{key} : {t1.elapsed()}')
+        print(f'{key1} : {t1.elapsed()}')
 
         y_values_bubble.append(t1.elapsed())
 
@@ -56,17 +66,17 @@ def main()-> None:
 
     print('selection sort')
 
-    for key, value in test_cases.items():
+    for key2, value2 in test_cases2.items():
 
         t2 = Timer()
         t2.start()
-        selection_sort(value)
+        selection_sort(value2)
         t2.stop()
 
         
         y_values_selection.append(t2.elapsed())
 
-        print(f'{key} : {t2.elapsed()}')
+        print(f'{key2} : {t2.elapsed()}')
 
     plt.plot(x_values, y_values_bubble, label='bubble')
     plt.plot(x_values, y_values_selection, label='selection')
