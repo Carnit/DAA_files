1687d6f3cdf56fbf24256650c3f2bd7b35d2eb4f
Carnit <nlesh.barat10.8@gmail.com>
2024-11-04 19:19:41 +0530

    edited alpha.lua

--- LAB_11/0_1kanpsack.c
+++ LAB_11/0_1kanpsack.c
@@ -1,14 +1,31 @@
-//wap for 0/1 kanpsack using Dynamic programmig
+// Wap for 0/1 kanpsack using Dynamic programming
 
 #include <stdio.h>
-#include <stdlib.h>
 
+int max(int a, int b) { return a > b ? a : b; }
+int kanpsack(int W, int wt[], int val[], int n) {
+  int i, w;
+  int K[n + 1][W + 1];
 
-void compare(int a,int b){
-  return ;
+  for (i = 0; i <= n; i++) {
+    for (w = 0; w <= W; w++) {
+      if (i == 0 || w == 0) {
+        K[i][w] = 0;
+      } else if (wt[i - 1] <= w) {
+        K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+      } else {
+        K[i][w] = K[i - 1][w];
+      }
+    }
+  }
+  return K[n][W];
 }
 
-int main(){
+int main() {
+  int profit[] = {60, 90, 120, 130, 80};
+  int weight[] = {10, 20, 30, 40, 50};
+  int W = 50;
+  int n = sizeof(profit) / sizeof(profit[0]);
+  printf("%d", kanpsack(W, weight, profit, n));
   return 0;
 }
-
